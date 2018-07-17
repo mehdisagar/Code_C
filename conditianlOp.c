886c0e0151e688d50da62e3b2b59041898b83692
Mehdi Sagar <mehdiqsagar@gmail.com>
2018-07-18 00:13:50 +0200

    conditianl operator

---
+++ conditianlOp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char first_name = 'P';
+    char last_name = 'F';
+    (last_name > first_name) ? printf("last_name") :
+                               printf("first_name");
+    return 0;
+}
