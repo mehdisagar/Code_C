2830bcb29e0d9a9d27d4b1583ebc7cbf8509180e
Mehdi Sagar <mehdiqsagar@gmail.com>
2018-07-21 17:14:15 +0200

    A simple game using Class in C++

---
+++ simpleGame/main.hpp
@@ -0,0 +1,76 @@
+/*header file for main.cpp*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Gameboard
+{
+    char gameSpace[4][4];
+public:
+    Gameboard(); //initialize the board with '-' in all 16 spaces
+    void setGameSpace(int row,int column, char value); //x,y,or '-' in each game square
+    char getGameSpace(int row,int column);
+    int fourInRow(); //four 'x's in any row 'wins'
+    void printInfo(); //print the game board in a 4x4 matrix
+};
+
+//TODO: complete the class definition
+
+Gameboard::Gameboard()
+{
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+        {
+            gameSpace[i][j] = '-';
+        }
+}
+
+void Gameboard::setGameSpace(int row, int column, char value)
+{
+    gameSpace[row][column] = value;
+}
+
+char Gameboard::getGameSpace(int row, int column){
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+            return gameSpace[i][j];
+        cout<<"\t";
+    }
+
+}
+
+
+int Gameboard::fourInRow()
+{
+    for(int row = 0; row < 4; row++){
+
+        int count = 0;
+        for(int col = 0; col < 4; col++)
+        {
+            if(gameSpace[row][col] == 'x')
+                count++;
+        }
+        if(count == 4) return 1;
+
+    }
+    return 0;
+
+
+}
+void Gameboard::printInfo()
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+            cout<< gameSpace[i][j] << "\t";
+        cout<<"\n";
+    }
+
+}
+
+
+
+
+
+
